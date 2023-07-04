b45eb8a0e268e8ba25ca810385e6422e5227e838
KenRouKoro <koro@korostudio.cn>
2023-07-04 20:17:11 +0800

    Initial commit

---
+++ src/main/java/cn/korostudio/yggdrasillite/YggdrasilLiteApplication.java
@@ -0,0 +1,13 @@
+package cn.korostudio.yggdrasillite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YggdrasilLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YggdrasilLiteApplication.class, args);
+    }
+
+}
